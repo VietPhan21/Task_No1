e6d2f8de2341f304b2f2ccdfff3d459c022770b3
Anh Thu Phan <minhviet.0970@gmail.com>
2022-09-17 22:28:06 +0300

    Program

---
+++ Program/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] table = {"Вафля", "Кот", "Уксус", "СO2", "-=+", "да", "Крот"};
+string[] table2 = new string [table.Length];
+
+{
+    int index = 0;
+    for (int i = 0; i < table.Length; i += 1)
+    {
+    if (table[i].Length <= 3)
+        {
+            table2[index] = table[i];
+            index += 1;
+        }
+    else
+        {
+            index += 1;
+        }
+    }
+}
+void PrintArray(string[] table)
+{
+    for (int i = 0; i < table.Length; i += 1)
+    {
+        Console.Write($"{table[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(table2);
